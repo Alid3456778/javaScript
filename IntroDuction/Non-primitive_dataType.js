e9d5d5290e50440e85fc9fff8e8669ef912a3192
Alid3456778 <AlidShaikh05@gmail.com>
2024-07-05 11:56:03 +0530

    operators and Expression

--- IntroDuction/Non-primitive_dataType.js
+++ IntroDuction/Non-primitive_dataType.js
@@ -1,11 +1,11 @@
 //non primitive data type is objet which consist of many variables 
 
 let item={
-    "name":"Alid",
-    "LastName":"Shaikh",
-    "age":34,
-    "address":"Pune",
-    "gender":"male"
+    name:"Alid",
+    LastName:"Shaikh",
+    age:34,
+    address:"Pune",
+    gender:"male"
 }
 
 console.log(item["name"]);
---
+++ IntroDuction/Operators_Expresion.js
@@ -0,0 +1,62 @@
+//Operators are some Symbols which are use to denote its format of doing job 
+    //+ operator mean it can add some value
+    //it consist all the symbol
+    
+    
+//Expression is an value used in operator , its the Answer of some Question
+
+
+
+let a=45,b=4;
+// console.log("a + b =", a+b);
+// console.log("a to the power of b :", a**b);//power
+
+
+
+
+
+        //Incremental amd Decremental Operator
+// console.log("a++ ",a++)
+// console.log("++a ",++a)//in long format a=a+1
+
+// console.log("b-- ",b--)
+// console.log("--b ",--b)
+
+
+
+
+
+        //Asignment Operators
+// a=4;
+// console.log(a);
+// a+=5;//a=a+5
+// console.log(a);
+
+
+
+
+        //Comperision Operators
+// let num1=4,num2=4 //num1="4"
+// console.log("num1 == num2",num1==num2)
+// console.log("num1 != num2",num1!=num2)
+
+// console.log("num1 === num2 ",num1 === num2)//it also check the data type == doesnt check the data type
+// console.log("num1 !== num2",num1 !== num2)
+
+
+
+
+
+        //Logical Operators 
+    //&& || !
+
+
+
+
+
+        //Bitwise Operator
+
+
+//+,-,* is operators and the number which is use known as operand
+
+
