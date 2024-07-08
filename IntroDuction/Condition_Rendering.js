124a491d4713677bebe27ae4fcecc38852ddf295
Alid3456778 <AlidShaikh05@gmail.com>
2024-07-08 19:52:47 +0530

    Conditional rendring

--- IntroDuction/Condition_Rendering.js
+++ IntroDuction/Condition_Rendering.js
@@ -1,8 +1,10 @@
 //its use to when we want to menupulate the output of data , it can change as per ower will
 //Use of "if, if else , else "
 
-let a=prompt("Enter the Number");
-a= Number.parseInt(a);//converting the string number into number
+// let a = prompt("Enter the Number")
+// a = Number.parseInt(a);//converting the string number into number
+
+let a =23;
 
 if(a<18){
     console.log("Use are not teen")
@@ -12,4 +14,23 @@ else if(a>18 && a<20){
 }
 else{
     console.log("You are Audelt")
+}
+
+let exe="dfd";
+
+switch(exe){
+    case "Mango":
+        console.log("You have  Mango")
+        break;
+    
+    case "Stobary":
+        console.log("You have  Stobary")
+        break;
+
+    case "Banana":
+        console.log("You have  banana")
+        break;
+
+    default:
+        console.log("YOu have not fruit")
 }
