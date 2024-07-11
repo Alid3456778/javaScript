d62778cc0b5d84810e9695ba7f73d6900916c46a
Alid3456778 <AlidShaikh05@gmail.com>
2024-07-11 22:01:17 +0530

    loop js

--- IntroDuction/Loop_Rendering.js
+++ IntroDuction/Loop_Rendering.js
@@ -1,1 +1,12 @@
 //there are various type of loop in js
+//for loop : it contains the starting and end point of loop 
+            //for loop is used to iterate over a block of code a number of times
+            //for in loop : it contains the object of loop
+            
+//while loop : it contains the condition of loop
+                //it is use for where the end is define but not the strat
+
+//do while loop :
+                    
+
+
